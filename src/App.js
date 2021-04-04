4e8deb5ca1c9f6431581131f7c17df84227ae42d
jmba909 <36124096+jmbiglangawa@users.noreply.github.com>
2021-04-04 11:14:11 +0800

    Styles reset
    Initial code is reset due to changing styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './App.scss'
 
-function App() {
-    return <div className="App">TEST</div>
+const App = () => {
+    return <div className="App"></div>
 }
 
 export default App
