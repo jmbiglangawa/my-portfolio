de9bb959294f755cf9c1b96ad1c8b831aa28d40a
swingspringer <parekoy201@gmail.com>
2022-08-08 10:21:43 +0800

    Chore: Setup projectLists and add link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,4 @@
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import styled from '@emotion/styled'
 import Wrapper from './Wrapper'
 import '../styles/Base.scss'
@@ -52,6 +53,12 @@ const ProjectTitle = styled.div`
 
     & h4 {
         display: inline-block;
+
+        & a svg {
+            margin-left: 5px;
+            color: #888888;
+            width: 20px;
+        }
     }
 
     & span {
@@ -63,46 +70,53 @@ const ProjectTitle = styled.div`
 
 const ProjectList = [
     {
-        banner: 'usada-kensetsu.png',
-        name: 'Usada Kensetsu',
-        desc:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget velit nisi. Ut ut ullamcorper nibh. Praesent venenatis tempus est ac lacinia.',
-        date: '08/02/2022',
-    },
-    {
         banner: 'ViReX-E.png',
         name: 'ViReX-E',
         desc:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget velit nisi. Ut ut ullamcorper nibh. Praesent venenatis tempus est ac lacinia.',
+            'As part of our thesis and our goal to save money, we repaired a broken Treadmill and re-used it as a Virtual Reality Exercise Equipment.',
         date: '03/05/2019',
+        link: 'https://youtu.be/9vOnzmItaFo',
+    },
+    {
+        banner: 'Maven-Module-Builder.png',
+        name: 'Maven Module Builder',
+        desc:
+            "This certain project before is utilizing Maven's multi-module setup. Building the project at once takes from 10-45 minutes. I trimmed it down to 1 to 5 minutes to improve work.",
+        date: '07/29/2020',
+        link: 'https://github.com/swingspringer/MavenModuleBuilder',
+    },
+    {
+        banner: 'usada-kensetsu.png',
+        name: 'Usada Kensetsu',
+        desc:
+            'A project I worked with people globally. This is primarily a fan website aimed as a joke construction firm website. I learned most of my React from my Argentina buddy here!',
+        date: '12/13/2020',
+        link: 'https://github.com/swingspringer/usada-kensetsu',
     },
     {
         banner: 'Cat-alogue.png',
         name: 'Cat-alogue',
         desc:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget velit nisi. Ut ut ullamcorper nibh. Praesent venenatis tempus est ac lacinia.',
+            'The project had a lot of logic that are non-programmer related, and requires many knowledge. None of it is documented so I tried to make this until they stopped me.',
         date: '08/30/2021',
-    },
-    {
-        banner: 'Maven-Module-Builder.png',
-        name: 'Cat-alogue',
-        desc:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget velit nisi. Ut ut ullamcorper nibh. Praesent venenatis tempus est ac lacinia.',
-        date: '08/30/2021',
+        link:
+            'https://www.figma.com/file/f5DS3rD7USz9wyUNi3xu3L/Cat-alogue?node-id=153%3A89',
     },
     {
         banner: 'Kube-Switcher.png',
-        name: 'Cat-alogue',
+        name: 'Kube Switcher',
         desc:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget velit nisi. Ut ut ullamcorper nibh. Praesent venenatis tempus est ac lacinia.',
-        date: '08/30/2021',
+            "The project this time was microservices and the provided laptop was very weak. We had to use our servers so I made Kube-Switcher make the team's work more efficient.",
+        date: '07/29/2022',
+        link: 'https://github.com/swingspringer/Kube-Switcher',
     },
     {
         banner: 'my-portfolio.png',
-        name: 'Cat-alogue',
+        name: 'My Portfolio',
         desc:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget velit nisi. Ut ut ullamcorper nibh. Praesent venenatis tempus est ac lacinia.',
-        date: '08/30/2021',
+            "Of course I'm counting in this portfolio in the list. I did my best to put in all of my love and passion here. Look forward to the projects I will be adding in this website!",
+        date: '08/29/2022',
+        link: '#',
     },
 ]
 
@@ -118,7 +132,15 @@ const Projects = () => {
                     <Banner id="banner" bg={'/img/banner/' + proj.banner} />
                     <Desc>
                         <ProjectTitle>
-                            <h4>{proj.name}</h4>
+                            <h4>
+                                {proj.name}
+                                <a
+                                    href={proj.link}
+                                    target="blank"
+                                    rel="noreferrer">
+                                    <OpenInNewIcon />
+                                </a>
+                            </h4>
                             <span>{proj.date}</span>
                         </ProjectTitle>
                         <p>{proj.desc}</p>
