8c0da322c73e4782b99003927e1cd53a32a6ec6e
swingspringer <parekoy201@gmail.com>
2022-08-08 18:21:34 +0800

    Chore: Cleanup

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import Home from './components/Home'
 const App = () => {
     return (
         <div className="App">
-            {/* <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="projects" element={<Projects />} />
-                    <Route path="contact" element={<Contact />} />
-                </Routes>
-            </BrowserRouter> */}
             <Home />
         </div>
     )
