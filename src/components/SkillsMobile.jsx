9788690c3cf6ac916b728f963b600e16e0a1b87a
swingspringer <parekoy201@gmail.com>
2022-08-29 16:48:51 +0800

    feature: Skills

--- src/components/SkillsMobile.jsx
+++ src/components/SkillsMobile.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
 import { Backend, Frontend } from './data/Skills'
+import Button from '@mui/material/Button'
+import classnames from 'classnames'
 
 const MobileWrapper = styled.div`
     display: flex;
@@ -20,14 +22,13 @@ const MobileScreen = styled.div`
     border-radius: 21px 21px 0px 0px;
     margin: 5px;
     padding: 20px 10px;
-    height: 90%;
+    height: 15%;
     display: flex;
     position: relative;
 `
 
 const GroupedApps = styled.div`
     margin: 5px;
-    background: url(${props => props.app});
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -43,26 +44,17 @@ const GroupedApps = styled.div`
     }
 `
 
-const GroupedAppModalWrapper = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    margin: 5px;
-    height: 90%;
-    width: 284px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
 const Overlay = styled.div`
     background-color: #000000;
     opacity: 0.5;
-    height: 100%;
-    width: 100%;
+    height: 90%;
+    width: 284px;
+    margin: 5px;
     position: absolute;
+    top: 0;
+    left: 0;
     border-radius: 21px 21px 0px 0px;
-    z-index: 1;
+    z-index: 0;
 `
 
 const GroupedAppModal = styled.div`
@@ -73,6 +65,8 @@ const GroupedAppModal = styled.div`
     padding: 10px;
     z-index: 2;
     text-align: center;
+    position: absolute;
+    left: 11%;
 
     p {
         margin-block-start: 0;
@@ -113,6 +107,57 @@ const App = styled.div`
     }
 `
 
+const AppOverview = styled.div`
+    background-color: #ffffff;
+    height: 85%;
+    width: 284px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: 5px;
+    margin-top: 30px;
+    z-index: 2;
+
+    img {
+        width: 50px;
+    }
+
+    p {
+        margin: 20px;
+    }
+
+    & .MuiButton-root.MuiButton-contained {
+        background-color: #767676;
+    }
+`
+
+const AppContents = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+const NavWrapper = styled.div`
+    opacity: 0.5;
+    bottom: 0;
+    left: 0;
+    width: 284px;
+    height: 41px;
+    position: absolute;
+    border-radius: 0px 0px 21px 21px;
+    z-index: 1;
+    margin: 5px;
+
+    display: flex;
+    justify-content: space-between;
+`
+
+const Nav = styled.div`
+    width: 33%;
+`
+
 const GroupedAppsList = [
     { image: 'back-end', label: 'Back-end', apps: Backend },
     { image: 'front-end', label: 'Front-end', apps: Frontend },
@@ -121,6 +166,19 @@ const GroupedAppsList = [
 const SkillsMobile = () => {
     const [group, setGroup] = useState()
     const [app, setApp] = useState()
+
+    const clearAll = () => {
+        setGroup(null)
+        setApp(null)
+    }
+
+    const backHandler = () => {
+        if (app) {
+            setApp(null)
+        } else if (group) {
+            setGroup(null)
+        }
+    }
 
     return (
         <MobileWrapper>
@@ -137,9 +195,12 @@ const SkillsMobile = () => {
                 </MobileScreen>
 
                 {group && (
-                    <GroupedAppModalWrapper>
-                        <GroupedAppModal>
-                            <p>Back-end</p>
+                    <>
+                        <GroupedAppModal
+                            className={classnames('animate__animated', {
+                                animate__zoomIn: group,
+                            })}>
+                            <p>{group.label}</p>
                             <Apps>
                                 {group.apps.map(backend => (
                                     <App
@@ -156,9 +217,35 @@ const SkillsMobile = () => {
                                 ))}
                             </Apps>
                         </GroupedAppModal>
-                        <Overlay />
-                    </GroupedAppModalWrapper>
+                        {!app && <Overlay onClick={() => setGroup(null)} />}
+                    </>
                 )}
+
+                {app && (
+                    <AppOverview>
+                        <AppContents
+                            className={classnames('animate__animated', {
+                                animate__zoomIn: app,
+                            })}>
+                            <img
+                                src={`${process.env.PUBLIC_URL}/img/skills/${app.png}.png`}
+                                alt="app-icon"></img>
+                            <h4>{app.label || app.png}</h4>
+                            <p>{app.output}</p>
+                            <Button
+                                variant="contained"
+                                onClick={() => setApp(null)}>
+                                Close
+                            </Button>
+                        </AppContents>
+                    </AppOverview>
+                )}
+
+                <NavWrapper>
+                    <Nav onClick={() => {}} /> {/* Tasks */}
+                    <Nav onClick={clearAll} /> {/* Desktop */}
+                    <Nav onClick={backHandler} /> {/* Back */}
+                </NavWrapper>
             </Mobile>
         </MobileWrapper>
     )
