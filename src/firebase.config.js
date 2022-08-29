1049e46edbb67b073e6eef16e6b04bbeadf56d36
swingspringer <parekoy201@gmail.com>
2022-08-29 17:30:38 +0800

    feature: Firebase integration

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_MEASURE_ID}`,
+}
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './firebase.config.js'
 import 'animate.css'
 
 const container = document.getElementById('root')
